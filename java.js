cbe99fcef1293e80dc555129eb1711d8b2bc14d5
moeed038ulazeem <moeedmoeed038@gamil.com>
2024-08-10 21:05:52 -0700

    practice problem

---
+++ java.js
@@ -0,0 +1,20 @@
+//Proble 01:
+function greetUser()
+{
+    console.log("Hello,Welcome to the class");
+}
+greetUser();
+
+//Problem 02:
+function sqyareOfTwoNumbers (x){
+    var square = x*x;
+    console.log(`square of the given numbers ${square}`);
+}
+sqyareOfTwoNumbers(2);
+
+//Proble 03 :
+function sumOfTwoNumbers(x,y){
+    var sum = x+y;
+    console.log(`sum of two given numbers ${sum}`);
+}
+sumOfTwoNumbers(2,4);
